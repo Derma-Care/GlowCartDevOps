ac9c34c8b181d9113a3baff4e6a5a84748e0fb8d
Sai Manikanta <ch.saimanikanta92@gmai.com>
2025-12-01 12:54:00 +0530

    updated

--- clinicadmin-service/src/main/java/com/glowkart/clinicadmin/dto/ProcedurePricingDTO.java
+++ clinicadmin-service/src/main/java/com/glowkart/clinicadmin/dto/ProcedurePricingDTO.java
@@ -20,7 +20,8 @@ public class ProcedurePricingDTO {
     private List<Map<String, List<String>>> postProcedureQA;
     
     private int sittings;
-    private int minTime; // NEW
+    private String minTime; // NEW
+
 
     private double price;
     private double discountPercentage;
@@ -34,7 +35,7 @@ public class ProcedurePricingDTO {
     private double clinicPay;
     private double finalCost;
 
-    private Instant offerStart;     // NEW
-    private Instant offerValidDate; // NEW
+    private String offerStart;      // <-- frontend sends ISO string
+    private String offerValidDate;  // <-- frontend sends ISO string
     private boolean offerActive;    // NEW
 }
