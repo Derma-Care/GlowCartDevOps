42c75631136b66258607a82ae5d9dd9e369687f5
Sai Manikanta <ch.saimanikanta92@gmai.com>
2026-01-06 14:24:09 +0530

    updated

--- customer-service/src/main/java/com/glowkart/customer/dto/ProcedurePackageDTO.java
+++ customer-service/src/main/java/com/glowkart/customer/dto/ProcedurePackageDTO.java
@@ -44,5 +44,6 @@ public class ProcedurePackageDTO {
     
     private double totalDiscountPercentage; // clinic + NGK
     private double totalDiscountAmount;     // clinic + NGK
+    private double totalDiscountedAmount;
 
 }
